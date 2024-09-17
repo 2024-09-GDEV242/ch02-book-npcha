614369110133b246704afb6f3086ed97ee74dc2d
npcha <139284703+npcha@users.noreply.github.com>
2024-09-17 17:27:07 -0400

    87 - add a printDetails method that displays formatted output for all private fields

--- Book.java
+++ Book.java
@@ -63,4 +63,14 @@ public class Book
     {
         System.out.println(title);
     }
+    
+    /**
+     * 
+     */
+    public void printDetails()
+    {
+        System.out.println("Title: " + title 
+        + ", Author: " + author 
+        + ", Pages: " + pages);
+    }
 }
